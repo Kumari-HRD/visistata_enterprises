62c5abbf81429c0f7c7d234fdf9f0add44ddc1d4
Kumari-HRD <149582188+Kumari-HRD@users.noreply.github.com>
2026-01-21 17:03:47 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -161,7 +161,7 @@ function Contact() {
                       <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                     </svg>
                   </div>
-                  <div>
+                  {/*<div>
                     <p class="font-semibold">Phone</p>
                     <a
                       href="tel:+919035920555"
@@ -169,7 +169,7 @@ function Contact() {
                     >
                       (+91) &nbsp; 9035920555
                     </a>
-                  </div>
+                  </div>*/}
                 </div>
                 <div className="flex gap-3">
                   <div className="contactIcon-background border-circle ">
